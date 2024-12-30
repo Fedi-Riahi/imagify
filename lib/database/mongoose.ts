3bcf155a94acb2a6cf38217efa4ca380efdad6bf
Fedi-Riahi <riahifedi@outlook.fr>
2024-12-30 13:49:08 +0100

    commit 9

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -3,38 +3,39 @@ import mongoose, { Mongoose } from "mongoose";
 const MONGODB_URL = process.env.MONGO_URL;
 
 interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
+    conn: Mongoose | null;
+    promise: Promise<Mongoose> | null;
 }
 
-// Define a type for the global object with mongoose property
+// Extend the global namespace to include mongoose
 declare global {
-  let mongoose: MongooseConnection | undefined;
+    namespace NodeJS {
+        interface Global {
+            mongoose: MongooseConnection;
+        }
+    }
 }
 
 let cached: MongooseConnection = global.mongoose || { conn: null, promise: null };
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+if (!global.mongoose) {
+    global.mongoose = cached;
 }
 
 export const connectToDatabase = async () => {
-  if (cached.conn) {
+    if (cached.conn) {
+        return cached.conn;
+    }
+    if (!MONGODB_URL) {
+        throw new Error("MongoDB URL is not defined");
+    }
+
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
+        dbName: "imagify",
+        bufferCommands: false,
+    }).then(mongoose => mongoose);
+
+    cached.conn = await cached.promise;
+
     return cached.conn;
-  }
-
-  if (!MONGODB_URL) {
-    throw new Error("MongoDB URL is not defined");
-  }
-
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: "imagify",
-      bufferCommands: false,
-    });
-
-  cached.conn = await cached.promise;
-
-  return cached.conn;
 };
